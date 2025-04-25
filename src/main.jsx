f9b15b281f70461bef6fc37821597e0aa7c15875
suraj savle <surajsavle143@gmail.com>
2025-04-25 21:18:34 +0530

    ready to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import GifProvider from "./context/GifContext";
+import GifProvider from "./context/GifContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
